7899877a6ebe4b93bf2c59eef8efbd1d883ddbb3
im foolish <532468158@qq.com>
2018-12-11 01:57:55 +0800

    rename dice_spliter

--- CQDicebot/dice_spliter.h
+++ CQDicebot/dice_spliter.h
@@ -1,3 +1,3 @@
 #pragma once
 
-bool base_split_dice(const std::string &str_input, std::string & str_output, bool detail_message);
+bool base_split_dice(const std::string &str_input, std::string & str_output, bool giving_detailed_message);
