f4ae9e9fbf7d881859e4130847677b6275c6d8a3
im foolish <532468158@qq.com>
2018-12-11 16:24:24 +0800

    Revert "opt dice_protocol"
    This reverts commit 250ab88dfcc280ab64144ca5bbf95cd6af55bdd7.

--- CQDicebot/dice_protocol.h
+++ CQDicebot/dice_protocol.h
@@ -16,7 +16,7 @@ public:
 	int dynamic_protocol_id;
 	std::string get_identifier() { return *(this->identifier); };
 	virtual std::string resolve_request(
-		std::string &message,
+		std::string message,
 		const int32_t i_AuthCode,
 		const int64_t uint64_fromGroupOrDiscuss,
 		const int64_t uint64_fromQQ,
