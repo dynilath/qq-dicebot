923177cff6dc6123455330686116313a86762b9a
im foolish <532468158@qq.com>
2018-12-14 06:48:47 +0800

    update build setting
    with env having vcpkg root set, write a hardtext vcpkg root is useless

--- scripts/generate.ps1
+++ scripts/generate.ps1
@@ -4,8 +4,8 @@ Set-Location $PSScriptRoot\..\  # enter the parent folder
 mkdir build\$config_type -ErrorAction SilentlyContinue  # create build folder if not exists
 Set-Location .\build\$config_type  # enter the build folder
 
-$vcpkg_root = "H:\github\vcpkg"
-$vcpkg_triplet = "my-x86-windows-static"
+$vcpkg_root = $env:VCPKG_ROOT
+$vcpkg_triplet = $env:VCPKG_TRIPLET
 
 cmake -G "Visual Studio 15 2017" -T "v141" `
     -DCMAKE_TOOLCHAIN_FILE="$vcpkg_root\scripts\buildsystems\vcpkg.cmake" `
