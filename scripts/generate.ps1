c8a1375031cfd22288ab6fcb39d6c35afc4b693d
im foolish <532468158@qq.com>
2020-02-19 13:09:37 +0800

    ci: edit build script

--- scripts/generate.ps1
+++ scripts/generate.ps1
@@ -4,8 +4,11 @@ Set-Location $PSScriptRoot\..\  # enter the parent folder
 if (-not (Test-Path -Path ".\build")) {
     New-Item -Path ".\build" -ItemType "directory"
 }
-if (-not (Test-Path -Path "build\test_db")) {
-    New-Item -Path "build\test_db" -ItemType "directory"
+if (-not (Test-Path -Path "build\test_db_1")) {
+    New-Item -Path "build\test_db_1" -ItemType "directory"
+}
+if (-not (Test-Path -Path "build\test_db_2")) {
+    New-Item -Path "build\test_db_2" -ItemType "directory"
 }
 Set-Location .\build  # enter the build folder
 
