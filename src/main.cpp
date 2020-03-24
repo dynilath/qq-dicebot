b0874c046bfb0559955f016a36af9815d0337559
dynilath <dynilath@qq.com>
2020-03-25 04:44:20 +0800

    style: formatting

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,16 @@
-#include <cqcppsdk/cqcppsdk.h>
+#include "cqcppsdk/cqcppsdk.h"
 
-#include "dicebot/dicebot.h"
 #include "./cqtool.h"
+#include "dicebot/dicebot.h"
 
 namespace cqlogging = cq::logging; // 用于日志
 namespace cqmessage = cq::message; // 提供封装了的 Message 等类
 
-void resolve_cap(event_info &ei, std::string const &raw_source, cq::Target target) noexcept;
+void resolve_cap(event_info &ei, std::string const &raw_source,
+                 cq::Target target) noexcept;
 
-// 插件入口，在静态成员初始化之后，app::on_initialize 事件发生之前被执行，用于配置 SDK 和注册事件回调
+// 插件入口，在静态成员初始化之后，app::on_initialize
+// 事件发生之前被执行，用于配置 SDK 和注册事件回调
 CQ_INIT {
     cq::on_enable([] {
         std::string dir = cq::dir::app();
@@ -20,7 +22,7 @@ CQ_INIT {
     cq::on_disable([] { dicebot::salvage(); });
 
     cq::on_private_message([](const cq::MessageEvent &e) {
-        if(!dicebot::utils::basic_event_filter(e.message)) return;
+        if (!dicebot::utils::basic_event_filter(e.message)) return;
         ::event_info ei(e.user_id);
         if (!dicebot::try_fill_nickname(ei)) {
             if (!get_nickname(ei, e.user_id)) return;
@@ -29,7 +31,7 @@ CQ_INIT {
     });
 
     cq::on_group_message([](const cq::GroupMessageEvent &e) {
-        if(!dicebot::utils::basic_event_filter(e.message)) return;
+        if (!dicebot::utils::basic_event_filter(e.message)) return;
         ::event_info ei(e.user_id, e.group_id, ::event_type::group);
         if (!dicebot::try_fill_nickname(ei)) {
             if (!get_group_nickname(ei, e.group_id, e.user_id)) return;
@@ -38,7 +40,7 @@ CQ_INIT {
     });
 
     cq::on_discuss_message([](const cq::DiscussMessageEvent &e) {
-        if(!dicebot::utils::basic_event_filter(e.message)) return;
+        if (!dicebot::utils::basic_event_filter(e.message)) return;
         event_info ei(e.user_id, e.discuss_id, event_type::group);
         if (!dicebot::try_fill_nickname(ei)) {
             if (!get_group_nickname(ei, e.discuss_id, e.user_id)) return;
@@ -47,7 +49,8 @@ CQ_INIT {
     });
 }
 
-void resolve_cap(event_info &ei, std::string const &raw_source, cq::Target target) noexcept {
+void resolve_cap(event_info &ei, std::string const &raw_source,
+                 cq::Target target) noexcept {
     try {
         std::string temp;
         if (!dicebot::message_pipeline(raw_source, ei, temp)) return;
@@ -55,9 +58,12 @@ void resolve_cap(event_info &ei, std::string const &raw_source, cq::Target targe
         msg.send(target);
     } catch (const cq::ApiError &err) {
 #ifdef _DEBUG
-        cqlogging::debug(u8"DICE", u8"调用失败，错误码：" + std::to_string(err.code));
+        cqlogging::debug(u8"DICE",
+                         u8"调用失败，错误码：" + std::to_string(err.code));
 #endif
     } catch (const std::exception &err) {
-        cqlogging::warning(u8"DICE", u8"发生std exception：" + std::string(err.what()) + u8"\n发送消息为：" + raw_source);
+        cqlogging::warning(u8"DICE",
+                           u8"发生std exception：" + std::string(err.what())
+                               + u8"\n发送消息为：" + raw_source);
     }
 }
