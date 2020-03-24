50116ce5046d0847a75fcd7180bde0503fb7d24f
dynilath <dynilath@qq.com>
2020-03-25 05:33:27 +0800

    feat: poker now has a tarot pre-set

--- src/dicebot/entity/poker.cpp
+++ src/dicebot/entity/poker.cpp
@@ -11,8 +11,8 @@ using namespace poker;
 constexpr size_t MAX_DECK_SIZE = 200;
 
 namespace {
-    constexpr char core_deck_name[] = "core";
-    constexpr char standard_deck_name[] = "standard";
+    constexpr char poker_core_deck_name[] = "core";
+    constexpr char poker_standard_deck_name[] = "standard";
 
     constexpr char poker_name_ascii[][14] = {
         "heart 2",   "Heart 3",   "Heart 4",   "Heart 5",    "Heart 6",
@@ -90,7 +90,10 @@ namespace {
         "club7",    "club8",    "club9",    "club10",    "clubj",
         "clubq",    "clubk",    "cluba"};
 
-    constexpr char major_akana_name[][30] = {
+    constexpr char tarot_deck_name[] = "tarot";
+    constexpr char tarot_major_deck_name[] = "major arcana";
+    constexpr char tarot_minor_deck_name[] = "minor arcana";
+    constexpr char major_arcana_card_names[][30] = {
         "1 The Magician",   "2 The High Priestess",
         "3 The Empress",    "4 The Emperor",
         "5 The Hierophant", "6 The Lovers",
@@ -102,6 +105,32 @@ namespace {
         "17 The Star",      "18 The Moon",
         "19 The Sun",       "20 Judgement",
         "21 The World",     "0 The Foul"};
+
+    constexpr char minor_arcana_card_names[][30] = {
+        "Ace of Wands",    "2 of Wands",     "3 of Wands",
+        "4 of Wands",      "5 of Wands",     "6 of Wands",
+        "7 of Wands",      "8 of Wands",     "9 of Wands",
+        "10 of Wands",     "Page of Wands",  "Knight of Wands",
+        "Queen of Wands",  "King of Wands",
+
+        "Ace of Coins",    "2 of Coins",     "3 of Coins",
+        "4 of Coins",      "5 of Coins",     "6 of Coins",
+        "7 of Coins",      "8 of Coins",     "9 of Coins",
+        "10 of Coins",     "Page of Coins",  "Knight of Coins",
+        "Queen of Coins",  "King of Coins",
+
+        "Ace of Cups",     "2 of Cups",      "3 of Cups",
+        "4 of Cups",       "5 of Cups",      "6 of Cups",
+        "7 of Cups",       "8 of Cups",      "9 of Cups",
+        "10 of Cups",      "Page of Cups",   "Knight of Cups",
+        "Queen of Cups",   "King of Cups",
+
+        "Ace of Swords",   "2 of Swords",    "3 of Swords",
+        "4 of Swords",     "5 of Swords",    "6 of Swords",
+        "7 of Swords",     "8 of Swords",    "9 of Swords",
+        "10 of Swords",    "Page of Swords", "Knight of Swords",
+        "Queen of Swords", "King of Swords",
+    };
 
     template <class T>
     struct c_array_trait {};
@@ -122,17 +151,22 @@ namespace {
     house_container_t diamond;
     house_container_t club;
     ::std::array<::std::string, 2> joker;
-    ::std::array<::std::string, 22> major_akana;
+    ::std::array<::std::string, 22> major_arcana;
+    ::std::array<::std::string, 56> minor_arcana;
 
     using p_type = typename ::std::add_pointer<::std::string>::type;
-
-    ::std::vector<p_type> poker_core_vec(52);
-    ::std::vector<p_type> poker_standard_vec(54);
 
     using nick_2_card_map = ::std::unordered_map<std::string, p_type>;
     nick_2_card_map card_nicks;
 
-    void creat_card_name_map() noexcept{
+    ::std::unordered_map<::std::string, ::std::vector<p_type>> preset_deck_map =
+        {{poker_core_deck_name, {}},
+         {poker_standard_deck_name, {}},
+         {tarot_deck_name, {}},
+         {tarot_major_deck_name, {}},
+         {tarot_minor_deck_name, {}}};
+
+    void creat_card_name_map() noexcept {
         constexpr size_t arr_len = heart.size();
 
         using p_2_containers_t =
@@ -140,22 +174,49 @@ namespace {
         ::std::vector<p_2_containers_t> poker_standard = {
             &heart, &spade, &diamond, &club};
 
+        // init poker decks
+        auto& poker_core_vec = preset_deck_map[poker_core_deck_name];
+        auto& poker_standard_vec = preset_deck_map[poker_standard_deck_name];
+
+        poker_core_vec.resize(arr_len * 4);
+        poker_standard_vec.resize(arr_len * 4 + 2);
         for (size_t i = 0; i < arr_len * 4; i++) {
             auto& p_poker = *(poker_standard[i / arr_len]);
             p_poker[i % arr_len] = poker_name[i];
+            poker_core_vec.push_back(&p_poker[i % arr_len]);
             poker_core_vec[i] = poker_standard_vec[i] = &p_poker[i % arr_len];
         }
 
         joker[0] = poker_name[52];
         joker[1] = poker_name[53];
-
         poker_standard_vec[52] = &joker[0];
         poker_standard_vec[53] = &joker[1];
 
-        for (size_t i = 0; i < 22; i++) {
-            major_akana[i] = major_akana_name[i];
+        // init arcana decks
+        auto& tarot_vec = preset_deck_map[tarot_deck_name];
+        auto& major_arcana_vec = preset_deck_map[tarot_major_deck_name];
+        auto& minor_arcana_vec = preset_deck_map[tarot_minor_deck_name];
+
+        constexpr size_t major_len =
+            c_array_trait<decltype(major_arcana_card_names)>::length;
+        constexpr size_t minor_len =
+            c_array_trait<decltype(minor_arcana_card_names)>::length;
+
+        major_arcana_vec.reserve(major_len);
+        minor_arcana_vec.reserve(minor_len);
+        tarot_vec.reserve(major_len + minor_len);
+        for (size_t i = 0; i < major_len; i++) {
+            major_arcana[i] = major_arcana_card_names[i];
+            tarot_vec.push_back(&major_arcana[i]);
+            major_arcana_vec.push_back(&major_arcana[i]);
+        }
+        for (size_t i = 0; i < minor_len; i++) {
+            minor_arcana[i] = minor_arcana_card_names[i];
+            tarot_vec.push_back(&major_arcana[i]);
+            minor_arcana_vec.push_back(&major_arcana[i]);
         }
 
+        // init poker nicks
         for (size_t i = 0;
              i < c_array_trait<decltype(poker_alter_names_set_1)>::length;
              i++) {
@@ -254,7 +315,7 @@ void poker_deck::init(const std::string& params) noexcept {
     this->clear();
     auto split_container = split_with_first_separator(params);
 
-    if(split_container.size() > MAX_DECK_SIZE)
+    if (split_container.size() > MAX_DECK_SIZE)
         this->card_sources.reserve(MAX_DECK_SIZE);
     else
         this->card_sources.reserve(split_container.size());
@@ -264,13 +325,9 @@ void poker_deck::init(const std::string& params) noexcept {
 
         std::string item_string = params.substr(item.first, item_len);
 
-        if (item_string == core_deck_name) {
-            for (auto& item : poker_core_vec) {
-                this->deck.push_back(item);
-            }
-            continue;
-        } else if (item_string == standard_deck_name) {
-            for (auto& item : poker_standard_vec) {
+        auto preset_d = preset_deck_map.find(item_string);
+        if (preset_d != preset_deck_map.end()) {
+            for (auto& item : preset_d->second) {
                 this->deck.push_back(item);
             }
             continue;
@@ -278,14 +335,12 @@ void poker_deck::init(const std::string& params) noexcept {
 
         auto [count, name] = split_into_number_and_name(params, item);
 
-        if(count > MAX_DECK_SIZE)
-            count = MAX_DECK_SIZE;
+        if (count > MAX_DECK_SIZE) count = MAX_DECK_SIZE;
         if (count + this->deck.size() > MAX_DECK_SIZE) {
             count = MAX_DECK_SIZE - this->deck.size();
         }
 
         if (count > 0) {
-
             std::string lc_content = utils::lower_case_copy(name);
             auto iter = card_nicks.find(lc_content);
 
