324066b7c83d9d64fc200983252f1ac8f2a9e053
dynilath <dynilath@qq.com>
2020-08-21 03:57:45 +0800

    fix: always mt19937

--- src/dicebot/random/random_provider.cpp
+++ src/dicebot/random/random_provider.cpp
@@ -12,33 +12,22 @@ static std::random_device rd_generator;
 static std::mt19937 mt_generator;
 
 void random::initialize() {
-    if (rd_generator.entropy() > 0.0) {
-        is_random_device_unavail = false;
-    } else {
-        mt_generator.seed(static_cast<unsigned int>(std::chrono::system_clock::now().time_since_epoch().count()));
-        is_random_device_unavail = true;
-    }
+    ::std::random_device rd;
+    ::std::seed_seq seed {rd(), rd(), rd(), rd(), rd(), rd(), rd(), rd()};
+    mt_generator.seed(seed);
 }
 
 int random::rand_int(int min, int max) {
-    if (min > max) min = max;
+    if (min > max) return max;
     std::uniform_int_distribution<int> dice_distribution(min, max);
-    if (is_random_device_unavail) {
-        return dice_distribution(mt_generator);
-    } else {
-        return dice_distribution(rd_generator);
-    }
+    return dice_distribution(mt_generator);
 }
 
-int random::rand_int(std::uniform_int_distribution<int>& distribution) {
-    if (is_random_device_unavail) {
-        return distribution(mt_generator);
-    } else {
-        return distribution(rd_generator);
-    }
+int random::rand_int(distribution_t const& distribution) {
+    return distribution(mt_generator);
 }
 
-std::uniform_int_distribution<int> random::create_distribution(int min, int max) {
+distribution_t random::create_distribution(int min, int max) {
     if (min > max) min = max;
-    return std::uniform_int_distribution<int>(min, max);
+    return distribution_t(min, max);
 }
--- src/dicebot/random/random_provider.h
+++ src/dicebot/random/random_provider.h
@@ -2,8 +2,9 @@
 #include <random>
 
 namespace dicebot::random {
+    using distribution_t = ::std::uniform_int_distribution<int>;
     void initialize();
     int rand_int(int min, int max);
-    int rand_int(std::uniform_int_distribution<int>&);
-    std::uniform_int_distribution<int> create_distribution(int min, int max);
+    int rand_int(distribution_t const&);
+    distribution_t create_distribution(int min, int max);
 } // namespace dicebot::random
