d90b981fba4f2b32e8035a2ae50092fb7cfc33b2
dynilath <532468158@qq.com>
2020-12-04 14:41:31 +0800

    perf: use xoroshiro128 generator

--- src/dicebot/random/random_provider.cpp
+++ src/dicebot/random/random_provider.cpp
@@ -3,28 +3,32 @@
 #include <chrono>
 #include <random>
 
+#include "../../utils/xoroshiro128starstar.hpp"
+
 using namespace dicebot;
 using namespace dicebot::random;
 
 static bool is_random_device_unavail = false;
 
-static std::random_device rd_generator;
-static std::mt19937 mt_generator;
+//static std::random_device rd_generator;
+//static std::mt19937 mt_generator;
+static xoroshiro128starstar xororshiro;
 
 void random::initialize() {
     ::std::random_device rd;
-    ::std::seed_seq seed {rd(), rd(), rd(), rd(), rd(), rd(), rd(), rd()};
-    mt_generator.seed(seed);
+    //::std::seed_seq seed {rd(), rd(), rd(), rd(), rd(), rd(), rd(), rd()};
+    //mt_generator.seed(seed);
+    xororshiro.seed(rd(),rd());
 }
 
 int random::rand_int(int min, int max) {
     if (min > max) return max;
     std::uniform_int_distribution<int> dice_distribution(min, max);
-    return dice_distribution(mt_generator);
+    return dice_distribution(xororshiro);
 }
 
 int random::rand_int(distribution_t const& distribution) {
-    return distribution(mt_generator);
+    return distribution(xororshiro);
 }
 
 distribution_t random::create_distribution(int min, int max) {
